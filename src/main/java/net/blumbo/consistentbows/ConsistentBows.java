b746461329fef64df071246d58091c70ecedb1aa
Blumbo <blumboman@gmail.com>
2022-11-26 21:08:41 +0200

    Initial commit

---
+++ src/main/java/net/blumbo/consistentbows/ConsistentBows.java
@@ -0,0 +1,9 @@
+package net.blumbo.consistentbows;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ConsistentBows implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/blumbo/consistentbows/client/ConsistentBowsClient.java
@@ -0,0 +1,13 @@
+package net.blumbo.consistentbows.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ConsistentBowsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
